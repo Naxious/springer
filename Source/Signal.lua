75ef08a01719bc1844ac173ce93705e45faee084
Cory Parsons <naxious@hotmail.com>
2025-03-01 08:06:26 -0500

    Update Signal.lua

--- Source/Signal.lua
+++ Source/Signal.lua
@@ -38,6 +38,46 @@ function Connection:Disconnect()
 	end
 end
 
+--[=[
+	@class Signal
+	@server
+	@client
+	@shared
+
+	Signals are a way to create a connection between two parts of your code. They are similar to events, but with a few key differences. Signals are not tied to any specific event, and can be fired at any time. They can also be waited on, which allows you to pause the execution of your code until the signal is fired.
+
+	```lua
+	local Signal = require(path.to.Signal)
+	local signal = Signal.new()
+
+	local connection = signal:Connect(function(...)
+		print("Signal fired with arguments:", ...)
+	end)
+
+	signal:Fire("Hello, world!")
+	connection:Disconnect()
+	```
+
+	Signals are useful for creating a decoupled system, where different parts of your code can communicate without needing to know about each other. They are also useful for creating a system where you can pause the execution of your code until a certain condition is met.
+
+	```lua
+	local Signal = require(path.to.Signal)
+	local signal = Signal.new()
+
+	local function waitForSignal()
+		return signal:Wait()
+	end
+
+	task.spawn(function()
+		waitForSignal()
+		print("Signal received!")
+	end)
+
+	task.delay(1)
+	signal:Fire()
+	```
+]=]
+
 local Signal = {}
 Signal.__index = Signal
 
